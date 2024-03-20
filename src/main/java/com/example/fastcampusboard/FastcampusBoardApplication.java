b26a32b4a04b6a2051bada3d614d0dba3d278822
Ahyoung <tictoc0124@gmail.com>
2024-03-20 23:18:15 +0900

    24.03.20
    공부하면서 작업한 것들.
    - 다음에는 더 자세히 쪼개어서 푸쉬하자.

--- src/main/java/com/example/fastcampusboard/FastcampusBoardApplication.java
+++ src/main/java/com/example/fastcampusboard/FastcampusBoardApplication.java
@@ -1,6 +1,11 @@
 package com.example.fastcampusboard;
 
+import com.example.fastcampusboard.domain.Article;
+import com.example.fastcampusboard.repository.ArticleCommentRepository;
+import com.example.fastcampusboard.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
@@ -11,9 +16,51 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @EntityScan(basePackages = {"com.example.fastcampusboard.domain"})
 @EnableAutoConfiguration(exclude={JpaRepositoriesAutoConfiguration.class})
 public class FastcampusBoardApplication {
-
+	//24.03.19
+	@Autowired
+	private static ArticleRepository articleRepository;
+	@Autowired
+	private static ArticleCommentRepository articleCommnetRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(FastcampusBoardApplication.class, args);
+
+
+		// select
+
+		//insert
+//	long previousCount = articleRepository.count();
+//	Article article = Article.of("new article","new content","new hashtag");
+//	Article savedArtice = articleRepository.save(article);
+
+		//update
+//		Article article = articleRepository.findById(1L).orElseThrow();
+//		String updatedHashtag ="#springboot";
+//
+//		article.setHashtag(updatedHashtag);
+//
+//		// 24.03.20
+//		// 영속성 컨텍스트으로부터 artice 값을 가져와서 save 한다.
+////		Article savedArtice = articleRepository.save(article);
+//		Article savedArtice = articleRepository.saveAndFlush(article);
+
+
+		//24.03.20
+		//delete
+		Article article = articleRepository.findById(1L).orElseThrow();
+		long previousArticleCount = articleRepository.count(); // 삭제 전, 글 갯수 확인
+		long previousArticleCommentCount = articleCommnetRepository.count(); // 글이 삭제 될 시, 댓글 수도 지워지니깐, 지워지기 전 댓글 수도 확인한다.
+
+		// 글이 삭제 될 때, 지워지는 댓글 수를 미리 확인한다.
+		// 양방향 바인딩을 통해 글의 댓글을 가져올 수 있다.
+		int deletedCommentsSize = article.getArticleComments().size();
+
+		articleRepository.delete(article);
+
 	}
+
+
+
+
+
 
 }
